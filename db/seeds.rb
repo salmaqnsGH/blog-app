f03d9a54c80ea6172b38062523f9215f0a6b68e9
salmaqnsGH <nurussalamahqonaah@gmail.com>
2022-08-19 11:22:48 +0700

    Integrated Technology model

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,3 +48,10 @@ end
     )
   end
 puts '9 Portfolio items created'
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+puts "3 Technologies created"
